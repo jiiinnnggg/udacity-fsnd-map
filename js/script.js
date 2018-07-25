66de21070bb813c4d9e013ee0db0eb9ac243bab2
Jing Sun <jing.sun@fundformer.com>
2018-07-25 11:36:01 -0400

    Incorporate filtered fuzzy search

--- js/script.js
+++ js/script.js
@@ -29,13 +29,32 @@ function mapViewModel() {
 	var self = this;
 
 	this.locationsList = ko.observableArray([]);
+
+	//query data-binds to the search input
 	this.query = ko.observable('');
 
 	places.forEach(function(item) {
 		self.locationsList.push( new Location(item) );
-
 	});
 
+	//computed observable for the filtered list
+	this.filteredList = ko.computed(function() {
+		var result = ko.observableArray([]);
+
+		if (this.query() == '') {
+			result = self.locationsList;
+		} else {
+			for (p in places) {
+				//if query (not case sensitive) is in p.name
+				if (places[p].name.toLowerCase().
+					indexOf(this.query().toLowerCase()) >= 0) {
+					result.push(places[p]);
+				}
+			}
+		}
+
+		return result();
+	}, this);
 
 };
 
